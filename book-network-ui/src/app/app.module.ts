7d8ed9640699808f81950659e45c6bd2fed60e45
mohamedbenrabah <mohamed.benrabah98@gmail.com>
2024-08-05 10:07:22 +0100

    activate account

--- book-network-ui/src/app/app.module.ts
+++ book-network-ui/src/app/app.module.ts
@@ -7,18 +7,22 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './pages/login/login.component';
 import {FormsModule} from "@angular/forms";
 import { RegisterComponent } from './pages/register/register.component';
+import {CodeInputModule} from "angular-code-input";
+import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ActivateAccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CodeInputModule
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
